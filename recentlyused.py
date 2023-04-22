bdd34624969f8a935d8a72a4855b31166dd9d0dd
Ben Findlay <67914781+teescdf@users.noreply.github.com>
2023-04-22 10:18:45 +0100

    Update recentlyused.py

--- recentlyused.py
+++ recentlyused.py
@@ -1,7 +1,7 @@
 # This script is designed to parse a recently-used.xbel file and extract each entry's URI, added, modified and visited date, and also produce the MD5 hash of the file URI
-# The script should be run against a recently-used.xbel file, using the command line structure of 'python linuxthumbparse.py /path/to/recently-used.xbel'. The data is then output into a CSV file called 'recent.csv'
-# The hash of the file URI can be cross referenced against the files found in a user's /cache/thumbnails directory to find matches 
-# When combined with file carving and PhotoDNA, the original file may be recovered, corroborated agianst the thumbnail image, and additional context and provneance given to it
+# The script should be run against a recently-used.xbel file, using the command line structure of 'python recentlyused.py /path/to/recently-used.xbel'. The data is then output into a CSV file called 'recent.csv'
+# The hash of the file URI can be cross referenced against the files found in the relevant user's $HOME/.cache/thumbnails directory to find matches 
+# When combined with file carving and PhotoDNA, the original file may be recovered, corroborated agianst the thumbnail image, and additional context and provneance given to it!
 import csv
 import hashlib
 import sys
